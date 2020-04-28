4960a54c727d21fd0073146b9fe44a86b5306129
Vadim Storozhuk <vadozy@yahoo.com>
2020-04-27 23:22:16 -0400

    End of Section 4

--- assignment-dockerfile/app.js
+++ assignment-dockerfile/app.js
@@ -39,3 +39,28 @@ process.on('unhandledRejection', (err) => {
 });
 
 init();
+
+// From sample-graceful-shutdown
+process.on('SIGINT', function onSigint () {
+	console.info('Got SIGINT (aka ctrl-c in docker). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+});
+
+// quit properly on docker stop
+process.on('SIGTERM', function onSigterm () {
+  console.info('Got SIGTERM (docker container stop). Graceful shutdown ', new Date().toISOString());
+  shutdown();
+})
+
+// shut down server
+function shutdown() {
+  // NOTE: server.close is for express based apps
+  // If using hapi, use `server.stop`
+  server.stop(function onServerClosed (err) {
+    if (err) {
+      console.error(err);
+      process.exitCode = 1;
+		}
+		process.exit();
+  })
+}
